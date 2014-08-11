025c5960f56b45c62edc3b79ae04dacc89369bb8
David Willie <david.willie.1@gmail.com>
2014-08-11 22:25:34 +1000

    Started inserting tags into cards and removing the bracketted text in the card body

--- pointy.js
+++ pointy.js
@@ -1,22 +1,40 @@
 getCardText = function(card) {
-    return card.getElementsByClassName("js-card-name")[0].innerHTML
+    return card.getElementsByClassName("js-card-name")[0].innerHTML;
+}
+
+setCardText = function(card, text) {
+    return card.getElementsByClassName("js-card-name")[0].innerHTML = text;
+}
+
+addTag = function(card, tagText) {
+    tag = document.createElement("span");
+    tag.classList.add("tag");
+    tag.innerHTML = tagText;
+
+    console.log("Adding tag");
+    card.innerHTML += tag.outerHTML
 }
 
 processList = function(list) {
-     = new RegExp(/\((.*)\)/)
-
-    cards = list.getElementsByClassName("list-cards");
+    scoreMatcher = new RegExp(/\((.*)\)/g);
+    cards = list.getElementsByClassName("list-card");
     for (var i = 0; i < cards.length; i++)
     {
-        cardText = cards[i].getElementsByClassName("js-card-name")[0].innerHTML
+        cardText   = getCardText(cards[i]);
+        regexMatch = scoreMatcher.exec(cardText);
+        if (regexMatch === null || regexMatch === undefined) {
+            continue;
+        }
 
+        cardScore  = regexMatch[1];
+        setCardText(cards[i], cardText.replace(regexMatch[0], ""));
+         addTag(cards[i], cardScore);
     }
 }
 
 main = function() {
     lists = document.getElementsByClassName("list");
     for (var i = 0; i < lists.length; i++) {
-        console.log("List!");
         processList(lists[i]);
     }
 
