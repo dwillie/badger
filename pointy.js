241763edff29f3dea30b5a432820c5010bc04df6
David Willie <david.willie.1@gmail.com>
2014-08-19 23:10:12 +1000

    Fixes/Further implementation of meta data structure wrapping around the trello board

--- pointy.js
+++ pointy.js
@@ -23,63 +23,123 @@ newTagContainer = function() {
 }
 
 getTagsContainer = function(card) {
-    tagsContainer = card.getElementsByClassName("tags-container")[0];
+    tagsContainer = card.node.getElementsByClassName("tags-container")[0];
     if (!tagsContainer) {
-        card.innerHTML += newTagContainer().outerHTML;
-        tagsContainer = card.getElementsByClassName("tags-container")[0];
+        card.node.innerHTML += newTagContainer().outerHTML;
+        tagsContainer = card.node.getElementsByClassName("tags-container")[0];
     }
     return tagsContainer;
 }
 
 getCardText = function(card) {
-    return card.getElementsByClassName("js-card-name")[0].innerHTML;
+    return card.node.getElementsByClassName("js-card-name")[0].innerHTML;
 }
 
 setCardText = function(card, text) {
-    return card.getElementsByClassName("js-card-name")[0].innerHTML = text;
+    card.node.getElementsByClassName("js-card-name")[0].innerHTML = text;
 }
 
-addTag = function(card, tagText) {
+addTag = function(card, tagText, iconName) {
     tagsContainer = getTagsContainer(card);
 
     tag = document.createElement("div");
     tag.classList.add("tag");
-    tag.innerHTML = awesomeIcon("trophy").outerHTML;
+    tag.innerHTML = awesomeIcon(iconName).outerHTML;
     tag.innerHTML += " " + tagText;
 
-    console.log("Adding tag");
     tagsContainer.innerHTML += tag.outerHTML;
 }
 
-processList = function(list) {
-    scoreMatcher = new RegExp(/\((.*)\)/g);
-    cards = list.getElementsByClassName("list-card");
-    for (var i = 0; i < cards.length; i++)
-    {
-        cardText   = getCardText(cards[i]);
-        regexMatch = scoreMatcher.exec(cardText);
-        if (regexMatch === null || regexMatch === undefined) {
-            continue;
-        }
+processTag = function(card, reg, iconName) {
+    cardText   = getCardText(card);
+    regexMatch = reg.exec(cardText);
+    if (!regexMatch) {
+        return;
+    }
 
-        cardScore  = regexMatch[1];
-        setCardText(cards[i], cardText.replace(regexMatch[0], ""));
-        addTag(cards[i], cardScore);
+    cardScore = regexMatch[1];
+    setCardText(card, cardText.replace(regexMatch[0], ""));
+    addTag(card, cardScore, iconName);
+}
+
+cleanTags = function(card) {
+    getTagsContainer(card).innerHTML = "";
+}
+
+cleanCard = function(card) {
+    setCardText(card, card.text);
+    cleanTags(card);
+}
+
+processList = function(list) {
+    scoreMatcher   = new RegExp(/\((.*)\)/g);
+    hashtagMatcher = new RegExp(/\#([a-zA-Z]+)/g);
+    for (var i = 0; i < list.cards.length; i++)
+    {
+        processTag(list.cards[i], scoreMatcher, "trophy");
+        processTag(list.cards[i], hashtagMatcher, "tag");
     }
 }
 
-refreshCycle = function() {
-    lists = document.getElementsByClassName("list");
-    for (var i = 0; i < lists.length; i++) {
-        processList(lists[i]);
+clearList = function(list) {
+    for (var i = 0; i < list.cards.length; i++)
+    {
+        cleanCard(list.cards[i]);
+    }
+}
+
+refreshCycle = function(meta) {
+    for (var i = 0; i < meta.lists.length; i++) {
+        clearList(meta.lists[i]);
     }
 
-    // setTimeout(main, 1000);
+    loadMeta(meta);
+
+    for (i = 0; i < meta.lists.length; i++) {
+        processList(meta.lists[i]);
+    }
+
+    console.log("Reloading...");
+    setTimeout(main, 2500);
+}
+
+loadMeta = function(meta) {
+    meta.lists = [];
+    var lists = document.getElementsByClassName("list");
+    for (var i = 0; i < lists.length; i++) {
+        if (lists[i].classList.contains("add-list")) {
+            continue;
+        }
+
+        var list = {
+            pageIndex: i,
+            node: lists[i],
+            cards: []
+        };
+
+        var cards = list.node.getElementsByClassName("list-card");
+        for (var j = 0; j < cards.length; j++) {
+            var card = {
+                node: cards[j]
+            };
+
+            card.text = getCardText(card);
+            card.node.innerHTML += newTagContainer().outerHTML;
+
+            list.cards.push(card);
+        }
+        meta.lists.push(list);
+    }
 }
 
 main = function() {
     loadFontAwesome();
-    refreshCycle();
+
+    var meta = {
+        lists: []
+    };
+    loadMeta(meta);
+    refreshCycle(meta);
 }
 
 window.onload = main
