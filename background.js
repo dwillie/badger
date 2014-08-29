38417e8ddfc3cc71b09ce3a94e29cb3fba31a6c2
Dave <david.willie.1@gmail.com>
2014-08-29 17:09:10 +1000

    Added the beginnings of the page action UI.

---
+++ background.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: 'trello.com/b/' },
+          })
+        ],
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});
---
+++ popup.js
@@ -0,0 +1,4 @@
+var configField = document.getElementById("config-field");
+var configEditor = ace.edit(configField);
+configEditor.setTheme("ace/theme/monokai");
+configEditor.getSession().setMode("ace/mode/javascript");
