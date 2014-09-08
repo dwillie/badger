3444c1ed0e371d717d54d9701f1560f5a8bbe9e2
Dave <david.willie.1@gmail.com>
2014-09-08 16:52:48 +1000

    Added customizability of badgerconfig through the pageaction ui

--- popup.js
+++ popup.js
@@ -1,4 +1,22 @@
 var configField = document.getElementById("config-field");
 var configEditor = ace.edit(configField);
 configEditor.setTheme("ace/theme/monokai");
-configEditor.getSession().setMode("ace/mode/javascript");
+configEditor.getSession().setMode("ace/mode/json");
+
+loadConfig = function() {
+   chrome.storage.sync.get("badgerConfig", function(result) {
+       alert(result.badgerConfig);
+       configEditor.setValue(result.badgerConfig);
+   });
+};
+
+saveConfig = function() {
+    chrome.storage.sync.set({ "badgerConfig": configEditor.getValue() });
+    chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
+        chrome.tabs.reload(arrayOfTabs[0].id);
+    });
+};
+
+document.getElementById("save-button").onclick = saveConfig;
+document.getElementById("cancel-button").onclick = loadConfig;
+loadConfig();
